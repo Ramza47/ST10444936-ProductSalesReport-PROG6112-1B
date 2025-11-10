e1e23f78906f6ff791126ae5fc6f4308dad15194
lab_services_student <lab_services_student@LabVM1829899.cg0z1w1d0zfehes5kfytapcb2e.jnbx.internal.cloudapp.net>
2025-11-10 13:28:32 +0200

    fifth Commit

--- test/productsalesreport/ProductSalesReportTest.java
+++ test/productsalesreport/ProductSalesReportTest.java
@@ -9,25 +9,40 @@ public class ProductSalesReportTest {
     
     public ProductSalesReportTest() {
     }
-
-    @Test
-    public void testCalculateTotalSales() {
-    }
-
-    @Test
-    public void testCalculateAverageSales() {
-    }
-
-    @Test
-    public void testCalculateMaximumSale() {
-    }
-
-    @Test
-    public void testCalculateMinimumSale() {
-    }
-
-    @Test
-    public void testMain() {
-    }
     
+    ProductSalesReport report = new ProductSalesReport();
+
+    @Test
+    public void CalculateTotalSales_ReturnTotalSales() {
+       // Arrange
+        double[][] productSales = {
+            {300, 150, 700},
+            {250, 200, 600}
+        };
+
+        // Act
+        double total = report.calculateTotalSales(productSales);
+
+        // Assert
+        assertEquals(2200.0, total, 0.001, "Total sales should be 2200.0"); 
+        
+    }
+
+    @Test
+    public void AvergeSales_ReturnsAverageProductSales() {
+        
+        // Arrange
+        double[][] productSales = {
+            {300, 150, 700},
+            {250, 200, 600}
+        };
+
+        // Act
+        double average = report.calculateAverageSales(productSales);
+
+        // Assert
+        assertEquals(366.6667, average, 0.001, "Average sales should be approximately 366.67");
+            
+        }
+
 }
